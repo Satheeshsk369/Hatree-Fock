ff395d1773386005b4d2187ac2d561d427d8fb99
Satheeshsk369 <satheeshsk3693@gmail.com>
2023-12-12 07:09:13 +0530

    Rust&Haskell

---
+++ hatree-fock-rs/target/debug/deps/hatree_fock_rs-43c30e7b73e136c3.d
@@ -0,0 +1,5 @@
+/home/satheeshkumar/Desktop/Hatree-Fock/hatree-fock-rs/target/debug/deps/libhatree_fock_rs-43c30e7b73e136c3.rmeta: src/main.rs
+
+/home/satheeshkumar/Desktop/Hatree-Fock/hatree-fock-rs/target/debug/deps/hatree_fock_rs-43c30e7b73e136c3.d: src/main.rs
+
+src/main.rs:
