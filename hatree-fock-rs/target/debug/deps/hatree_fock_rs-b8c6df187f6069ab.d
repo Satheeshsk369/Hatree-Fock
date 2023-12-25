3d35c22eb277a5cec3db7c34b09ab62092408a7b
Satheeshsk369 <satheeshsk3693@gmail.com>
2023-12-25 06:45:50 +0530

    Test

--- hatree-fock-rs/target/debug/deps/hatree_fock_rs-b8c6df187f6069ab.d
+++
@@ -1,5 +0,0 @@
-/home/satheeshkumar/Desktop/Hatree-Fock/hatree-fock-rs/target/debug/deps/libhatree_fock_rs-b8c6df187f6069ab.rmeta: src/main.rs
-
-/home/satheeshkumar/Desktop/Hatree-Fock/hatree-fock-rs/target/debug/deps/hatree_fock_rs-b8c6df187f6069ab.d: src/main.rs
-
-src/main.rs:
