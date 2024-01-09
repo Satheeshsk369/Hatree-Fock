cbdb9e5be094cacc9d72e2fe11d1f2f91472c63d
Satheeshsk369 <satheeshsk3693@gmail.com>
2024-01-09 06:50:47 +0530

    Jupyter Plot

--- rust/src/functions.rs
+++
@@ -1,7 +0,0 @@
-pub fn linspace(start:f64,stop:f64,num:i64)->Vec<f64>{
-	let mut value: Vec<f64> = vec![];
-	for i in 0..num{
-		value.push(start * (i as f64) * (stop - start)/(num as f64));
-	}
-	return value;
-}
